8057da6ec61c415913b553b1e3c79d306298df63
Aayush Kapoor <aayushkapoor34@gmail.com>
2025-07-05 12:34:08 -0400

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "sonner"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -38,6 +39,7 @@ export default function RootLayout({
           {children}
           <Toaster position="bottom-right" richColors />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
